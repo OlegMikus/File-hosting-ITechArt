dc34b8c5180fc4deef24c8541b1ad2fec67b401d
OlegMikus <aleh.mikus@itechart-group.com>
2021-09-22 12:11:21 +0300

    add linter and base scripts for pipenv

---
+++ scripts/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo ">>> $(basename ${BASH_SOURCE[0]})"
+
+set -o errexit # exit script when command fails
+set -o pipefail # this setting prevents errors in a pipeline from being masked
+set -o nounset # exit script when it tries to use undeclared variables
+
+# INIT WORKING DIR
+# ===================================================
+cd "$(dirname "${BASH_SOURCE[0]}")"
+FILE_DIR=$(pwd)
+cd ..
+CWD="$(pwd)"
+
+docker-compose -f docker-compose.yml up
