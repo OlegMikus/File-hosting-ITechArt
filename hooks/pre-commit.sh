9eeda7b4a90057d4e57ed9e559e7e5b5b73568fb
OlegMikus <aleh.mikus@itechart-group.com>
2021-09-22 12:09:04 +0300

    add pre commit and pre push hooks

---
+++ hooks/pre-commit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo ">>> $(basename ${BASH_SOURCE[0]})"
+
+set -o errexit # exit script when command fails
+set -o pipefail # this setting prevents errors in a pipeline from being masked
+set -o nounset # exit script when it tries to use undeclared variables
+
+
+
+# INIT WORKING DIR
+# ===================================================
+cd "$(dirname "${BASH_SOURCE[0]}")"
+FILE_DIR=$(pwd)
+cd ..
+cd ..
+CWD="$(pwd)"
+
+chmod ug+x "${CWD}/scripts/lint.sh"
+"${CWD}"/scripts/lint.sh
