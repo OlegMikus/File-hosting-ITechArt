10dfffb1efbf9f94642c2cc5e05954481affa4f4
OlegMikus <aleh.mikus@itechart-group.com>
2021-09-23 12:04:38 +0300

    create User model

--- src/accounts/models/user.py
+++ src/accounts/models/user.py
@@ -11,3 +11,32 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+class User(BaseModel):
+    first_name = models.CharField(max_length=30,
+                                  help_text='Users first name')
+
+    last_name = models.CharField(max_length=40,
+                                 help_text='Users last name')
+
+    email = models.EmailField(max_length=254,
+                              unique=True,
+                              help_text='Users email')
+
+    age = models.IntegerField(help_text='')
+
+    password = models.CharField(max_length=64,
+                                help_text='Users password')
+
+    is_staff = models.BooleanField(
+        default=False,
+        help_text='Designates whether the user can log into this admin site.'
+    )
+    is_active = models.BooleanField(
+        default=True,
+        help_text='Designates whether this user should be treated as active. '
+                  'Unselect this instead of deleting accounts.')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
