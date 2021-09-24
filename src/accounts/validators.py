2ee6514cfb34186f8020df84cf261424d134844c
OlegMikus <aleh.mikus@itechart-group.com>
2021-09-24 12:54:42 +0300

    fix validators

--- src/accounts/validators.py
+++ src/accounts/validators.py
@@ -1,24 +1,18 @@
+import re
+
 from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
 
 
 def validate_age(value: int) -> None:
-
-    if value < 5 or value > 110:
-
+    if not 5 < value < 110:
         raise ValidationError(
-            _(f'{value} cannot be an age'),
-            params={'value': value},
+            f'{value} cannot be an age',
         )
 
 
 def validate_name(value: str) -> None:
-
-    allowed = 'abcdefghijklmnopqrstuvwxyz'
-    for symbol in value:
-        if symbol.lower() not in allowed:
-
-            raise ValidationError(
-                _(f'{value} cannot be an age'),
-                params={'value': value},
-            )
+    regex = r"^[a-zA-z]+$"
+    if re.match(regex, value):
+        raise ValidationError(
+            f'{value} is not valid phrase'
+        )
