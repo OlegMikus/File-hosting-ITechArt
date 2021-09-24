02dd7d78a96367487aa3d29eb4be5fd2c420a870
OlegMikus <aleh.mikus@itechart-group.com>
2021-09-24 13:06:50 +0300

    fix str

--- src/accounts/models/user.py
+++ src/accounts/models/user.py
@@ -40,4 +40,4 @@ class User(BaseModel, AbstractUser):
     objects = CustomUserManager()
 
     def __str__(self) -> str:
-        return f'{self.id} {self.first_name} {self.last_name}'
+        return f'User {self.id}: {self.first_name} {self.last_name}'
