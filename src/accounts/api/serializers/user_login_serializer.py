d08514da1125814d8f1c83c6432868bc928b1200
OlegMikus <aleh.mikus@itechart-group.com>
2021-09-29 14:38:00 +0300

    pretty code

--- src/accounts/api/serializers/user_login_serializer.py
+++ src/accounts/api/serializers/user_login_serializer.py
@@ -15,8 +15,8 @@ class UserLoginSerializer(serializers.ModelSerializer):
         fields = ('id', 'email', 'password')
 
     def validate(self, data):
-        email = data.get('email', None)
-        password = data.get('password', None)
+        email = data.get('email')
+        password = data.get('password')
         user = authenticate(email=email, password=password)
         if user is None:
             raise serializers.ValidationError(
