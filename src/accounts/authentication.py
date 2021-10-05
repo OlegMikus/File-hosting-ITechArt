061c4e8ad91876a462abf97eb1623b8254465f29
OlegMikus <aleh.mikus@itechart-group.com>
2021-10-05 11:41:39 +0300

    fix raise, change structure

--- src/accounts/authentication.py
+++ src/accounts/authentication.py
@@ -1,38 +1,30 @@
 from functools import wraps
 from typing import Any
 import jwt
-from rest_framework import exceptions, status
-from rest_framework.response import Response
 
 from src.accounts.models import User
+from src.base.services.std_error_handler import BadRequestError, ForbiddenError
 from src.config.env_consts import DJANGO_SECRET_KEY
 
 
 def login_required(func: Any) -> Any:
     @wraps(func)
-    def decorated(*args: Any, **kwargs: Any) -> Response:
+    def decorated(*args: Any, **kwargs: Any) -> Any:
 
         access_token = args[1].headers.get('access_token')
 
         if not access_token:
-            return None
+            raise BadRequestError('Missing token')
         try:
             payload = jwt.decode(
                 access_token, DJANGO_SECRET_KEY, algorithms=['HS256'])
+            user = User.objects.filter(id=payload.get('id')).first()
+
+            if user and user.is_active:
+                return func(*args, **kwargs)
 
         except jwt.ExpiredSignatureError as expired_signature:
-            response = {
-                'success': 'False',
-                'status code': status.HTTP_403_FORBIDDEN,
-                'message': 'Access token expired',
-                'redirect': 'http:0.0.0.0:8000/api/refresh'
-            }
-            raise exceptions.AuthenticationFailed(response) from expired_signature
-        except IndexError as index_error:
-            raise exceptions.AuthenticationFailed('Token prefix missing') from index_error
-
-        user = User.objects.filter(id=payload['id']).first()
-        if user and user.is_active:
-            return func(*args, **kwargs)
-
+            raise ForbiddenError(expired_signature) from expired_signature
+        except jwt.InvalidSignatureError as invalid_signature:
+            raise ForbiddenError(invalid_signature) from invalid_signature
     return decorated
