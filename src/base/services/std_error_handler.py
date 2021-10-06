8e6765217c18566e525f2c707f648953ddb743c0
OlegMikus <aleh.mikus@itechart-group.com>
2021-10-06 11:17:55 +0300

    add ValidationError, change typing

--- src/base/services/std_error_handler.py
+++ src/base/services/std_error_handler.py
@@ -1,6 +1,7 @@
-from typing import Any, Dict
+from typing import Any, Dict, Tuple
 
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
 
@@ -19,13 +20,13 @@ class NotFoundError(Exception):
         super().__init__(args[0])
 
 
-def generate_response(exc: Exception = None,
+def generate_response(error_detail: Tuple[Any, ...] = None,
                       state: status = None,
                       result: Dict[str, Any] = None,
                       total_count: int = 0) -> Dict[str, Any]:
     return {'data': {
         'status': state,
-        'error_detail': str(exc),
+        'error_detail': error_detail,
         'result': result,
         'total_count': total_count
     }}
@@ -33,10 +34,14 @@ def generate_response(exc: Exception = None,
 
 def std_error_handler(exc: Exception, context: Any) -> Response:
     if isinstance(exc, BadRequestError):
-        return Response(generate_response(exc=exc, state=status.HTTP_400_BAD_REQUEST))
+        print(exc.args)
+        return Response(generate_response(error_detail=exc.args, state=status.HTTP_400_BAD_REQUEST))
 
     if isinstance(exc, NotFoundError):
-        return Response(generate_response(exc=exc, state=status.HTTP_404_NOT_FOUND))
+        return Response(generate_response(error_detail=exc.args, state=status.HTTP_404_NOT_FOUND))
 
     if isinstance(exc, ForbiddenError):
-        return Response(generate_response(exc=exc, state=status.HTTP_403_FORBIDDEN))
+        return Response(generate_response(error_detail=exc.args, state=status.HTTP_403_FORBIDDEN))
+
+    if isinstance(exc, ValidationError):
+        return Response(generate_response(error_detail=exc.args, state=status.HTTP_400_BAD_REQUEST))
