001501931836f08da32cd718259ff1057b3a9741
OlegMikus <aleh.mikus@itechart-group.com>
2021-10-06 11:16:35 +0300

    fix responses with generate response function

--- src/base/services/responses.py
+++ src/base/services/responses.py
@@ -3,45 +3,16 @@ from typing import Dict, Any
 from rest_framework import status
 from rest_framework.response import Response
 
+from src.base.services.std_error_handler import generate_response
+
 
 class CreatedResponse(Response):
 
     def __init__(self, data: Dict[str, Any]) -> None:
-        data = {
-            'id': data.get('id'),
-            'email': data.get('email'),
-            'age': data.get('age'),
-        }
-        data_content = {
-            'data': {
-                'status': status.HTTP_201_CREATED,
-                'error_detail': None,
-                'result': data,
-                'total_count': 1
-            }
-        }
-        super().__init__(data_content)
+        super().__init__(generate_response(state=status.HTTP_201_CREATED, result=data, total_count=1))
 
 
 class OkResponse(Response):
 
     def __init__(self, data: Dict[str, str] = None) -> None:
-        if 'access_token' in data.keys():
-            data = {
-                'user_id': data.get('id'),
-                'access_token': data.get('access_token'),
-                'refresh_token': data.get('refresh_token')
-            }
-        else:
-            data = {
-                'user_id': data.get('id')
-            }
-        data_content = {
-            'data': {
-                'status': status.HTTP_200_OK,
-                'error_detail': None,
-                'result': data,
-                'total_count': 1
-            }
-        }
-        super().__init__(data_content)
+        super().__init__(generate_response(state=status.HTTP_200_OK, result=data, total_count=1))
