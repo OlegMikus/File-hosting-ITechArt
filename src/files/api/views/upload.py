3b1dad930113ef331d063729b389a9db69337240
OlegMikus <aleh.mikus@itechart-group.com>
2021-10-08 11:47:18 +0300

    dummy endpoint to check that it works

--- src/files/api/views/upload.py
+++ src/files/api/views/upload.py
@@ -1,5 +1,12 @@
+from typing import Any
+
 from rest_framework.generics import GenericAPIView
+from rest_framework.request import Request
+
+from src.base.services.responses import OkResponse
 
 
 class UploadView(GenericAPIView):
-    pass
+
+    def get(self, request: Request, *args: Any, **kwargs: Any) -> OkResponse:
+        return OkResponse({'message': 'Upload view working'})
