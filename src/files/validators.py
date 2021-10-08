47ee970ff3290b284f967d065d6b58afb7ac3082
OlegMikus <aleh.mikus@itechart-group.com>
2021-10-08 11:26:14 +0300

    fix raise message

--- src/files/validators.py
+++ src/files/validators.py
@@ -7,5 +7,4 @@ def file_type_validator(value: str) -> None:
     allowed_file_formats = ('.jpg', '.gif', '.tiff', '.png', '.svg', '.docx', '.xls', '.pdf', '.txt')
     if os.path.splitext(value)[1] not in allowed_file_formats:
         raise ValidationError(
-            'Unsupported file format'
-        )
+            'Unsupported format, select one of the following: jpg, gif, tiff, png, svg, docx, xls, pdf, txt')
