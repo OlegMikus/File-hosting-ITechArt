8de9d0ebf292ce0f0e9ef085c806463e81ee67c5
OlegMikus <aleh.mikus@itechart-group.com>
2021-10-15 17:14:10 +0300

    undo mediafile settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -100,9 +100,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
-MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/media/'
-
 AUTH_USER_MODEL = 'accounts.User'
 
 REST_FRAMEWORK = {
