e206c6e615fc8ab9b88162379814a857d7eda82a
OlegMikus <aleh.mikus@itechart-group.com>
2021-10-15 18:00:06 +0300

    update file model

--- src/files/models/file.py
+++ src/files/models/file.py
@@ -2,12 +2,14 @@ from django.db import models
 
 from src.accounts.models import User
 from src.base.models import BaseModel
+from src.files.models.files_storage import FilesStorage
 
 
 class File(BaseModel):
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    destination = models.ForeignKey(FilesStorage, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=256)
     description = models.TextField()
-    file = models.FileField(upload_to='file_storage/')
-
-    def __str__(self) -> str:
-        return f'File id in database: {self.id}'
+    type = models.CharField(max_length=10)
+    size = models.BigIntegerField()
+    hash = models.CharField(max_length=32)
