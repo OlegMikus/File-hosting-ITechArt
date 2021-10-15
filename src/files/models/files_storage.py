66b3602fd104232b3ea086509b42ea277edbfa24
OlegMikus <aleh.mikus@itechart-group.com>
2021-10-15 19:09:07 +0300

    fix field

--- src/files/models/files_storage.py
+++ src/files/models/files_storage.py
@@ -8,5 +8,5 @@ class FilesStorage(BaseModel):
         ('IS', 'INTERMEDIATE_STORAGE'),
         ('PS', 'PERMANENT_STORAGE')
     ]
-    storage_type = models.CharField(choices=STORAGE_CHOICES, default='IS')
+    storage_type = models.CharField(max_length=32, choices=STORAGE_CHOICES, default='IS',)
     destination = models.CharField(max_length=32)
