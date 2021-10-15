20bcc990fcdf48a5ef9dcad63823cb44747977bd
OlegMikus <aleh.mikus@itechart-group.com>
2021-10-15 17:16:09 +0300

    fix status

--- src/base/services/std_error_handler.py
+++ src/base/services/std_error_handler.py
@@ -21,11 +21,9 @@ class NotFoundError(Exception):
 
 
 def generate_response(error_detail: Tuple[Any, ...] = None,
-                      state: status = None,
                       result: Dict[str, Any] = None,
                       total_count: int = 0) -> Dict[str, Any]:
     return {'data': {
-        'status': state,
         'error_detail': error_detail,
         'result': result,
         'total_count': total_count
@@ -34,13 +32,13 @@ def generate_response(error_detail: Tuple[Any, ...] = None,
 
 def std_error_handler(exc: Exception, context: Any) -> Response:
     if isinstance(exc, BadRequestError):
-        return Response(generate_response(error_detail=exc.args, state=status.HTTP_400_BAD_REQUEST))
+        return Response(generate_response(error_detail=exc.args), status=status.HTTP_400_BAD_REQUEST)
 
     if isinstance(exc, NotFoundError):
-        return Response(generate_response(error_detail=exc.args, state=status.HTTP_404_NOT_FOUND))
+        return Response(generate_response(error_detail=exc.args), status=status.HTTP_404_NOT_FOUND)
 
     if isinstance(exc, ForbiddenError):
-        return Response(generate_response(error_detail=exc.args, state=status.HTTP_403_FORBIDDEN))
+        return Response(generate_response(error_detail=exc.args), status=status.HTTP_403_FORBIDDEN)
 
     if isinstance(exc, ValidationError):
-        return Response(generate_response(error_detail=exc.args, state=status.HTTP_400_BAD_REQUEST))
+        return Response(generate_response(error_detail=exc.args), status=status.HTTP_400_BAD_REQUEST)
