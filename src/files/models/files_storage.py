acadbfcc09ad58ecf9f0850063ace118a7786b58
OlegMikus <aleh.mikus@itechart-group.com>
2021-10-15 18:01:24 +0300

    create file storage model

---
+++ src/files/models/files_storage.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from src.base.models import BaseModel
+
+
+class FilesStorage(BaseModel):
+    STORAGE_CHOICES = [
+        ('IS', 'INTERMEDIATE_STORAGE'),
+        ('PS', 'PERMANENT_STORAGE')
+    ]
+    storage_type = models.CharField(choices=STORAGE_CHOICES, default='IS')
+    destination = models.CharField(max_length=32)
