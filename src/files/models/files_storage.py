05a25c7fe077742a97aee6346346f9bed4715781
OlegMikus <aleh.mikus@itechart-group.com>
2021-10-18 13:39:21 +0300

    move meta class under the fields add change choices

--- src/files/models/files_storage.py
+++ src/files/models/files_storage.py
@@ -1,14 +1,16 @@
 from django.db import models
 
 from src.base.models import BaseModel
+from src.files.constants import FILE_STORAGE__TYPE__TEMP, FILE_STORAGE__TYPE__PERMANENT
 
 
 class FilesStorage(BaseModel):
+
+    storage_type = models.CharField(max_length=32, choices=(
+        (FILE_STORAGE__TYPE__TEMP, 'Temporary Storage'),
+        (FILE_STORAGE__TYPE__PERMANENT, 'Permanent Storage'),
+    ))
+    destination = models.CharField(max_length=32)
+
     class Meta:
         db_table = 'files_storage'
-    STORAGE_CHOICES = [
-        ('temp', 'temp'),
-        ('permanent', 'permanent')
-    ]
-    storage_type = models.CharField(max_length=32, choices=STORAGE_CHOICES)
-    destination = models.CharField(max_length=32)
