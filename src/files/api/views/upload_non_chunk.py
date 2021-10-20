867525e98fa1246404a636a6daa14cea0ce799bc
OlegMikus <aleh.mikus@itechart-group.com>
2021-10-20 14:47:09 +0300

    improve non chunk upload view

--- src/files/api/views/upload_non_chunk.py
+++ src/files/api/views/upload_non_chunk.py
@@ -8,6 +8,10 @@ from rest_framework.response import Response
 from src.accounts.authentication import login_required
 from src.base.services.responses import CreatedResponse
 from src.base.services.std_error_handler import BadRequestError
+from src.files.constants import FILE_STORAGE__TYPE__TEMP, FILE_STORAGE__TYPE__PERMANENT
+from src.files.hash_count import hash_count
+from src.files.models import File
+from src.files.models.files_storage import FilesStorage
 
 
 class NonChunkUploadView(GenericAPIView):
@@ -16,10 +20,32 @@ class NonChunkUploadView(GenericAPIView):
     def post(self, request: Request, *args: Any, **kwargs: Any) -> Response:
         if not request.FILES.get('file'):
             raise BadRequestError()
+        storage = FilesStorage.objects.get(type=FILE_STORAGE__TYPE__PERMANENT)
+        user = kwargs.get('user')
+        description = request.data.get('description')
         file_data = request.FILES.get('file')
-        storage_path = os.path.expandvars('storage/permanent_storage/')
-        to_file_path = os.path.join(storage_path, file_data.name)
+        storage_dir = os.path.join(storage.destination, str(user.id))
+
+        if not os.path.isdir(storage_dir):
+            os.makedirs(storage_dir)
+
+        to_file_path = os.path.join(storage_dir, file_data.name)
+
         with open(to_file_path, 'wb+') as file:
             for chunk in file_data.chunks():
                 file.write(chunk)
-        return CreatedResponse()
+
+        files_hash = hash_count(to_file_path)
+        print(files_hash)
+
+        file = File.objects.create(user=user,
+                                   storage=storage,
+                                   destination=to_file_path,
+                                   name=file_data.name[0:-4],
+                                   description=description,
+                                   type=file_data.name[-4:],
+                                   size=file_data.size,
+                                   hash=files_hash)
+        file.save()
+
+        return CreatedResponse({'response': 'nice'})
