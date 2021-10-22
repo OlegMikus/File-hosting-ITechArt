205b16620ee5a50151c6ee5eb07486fe1146d6fc
OlegMikus <aleh.mikus@itechart-group.com>
2021-10-22 17:12:56 +0300

    fix responses

--- src/base/services/responses.py
+++ src/base/services/responses.py
@@ -8,14 +8,14 @@ from src.base.services.std_error_handler import generate_response
 
 class CreatedResponse(Response):
 
-    def __init__(self, data: Dict[str, Any] = None) -> None:
+    def __init__(self, data: Dict[str, Any]) -> None:
         super().__init__(data=generate_response(result=data, total_count=1),
                          status=status.HTTP_201_CREATED)
 
 
 class OkResponse(Response):
 
-    def __init__(self, data: Dict[str, str] = None) -> None:
+    def __init__(self, data: Dict[str, str]) -> None:
 
         super().__init__(data=generate_response(result=data, total_count=1),
                          status=status.HTTP_200_OK,)
@@ -23,6 +23,6 @@ class OkResponse(Response):
 
 class NotFoundResponse(Response):
 
-    def __init__(self, data: Dict[str, str] = None) -> None:
+    def __init__(self, data: Dict[str, str]) -> None:
         super().__init__(data=generate_response(result=data, total_count=0),
                          status=status.HTTP_404_NOT_FOUND)
