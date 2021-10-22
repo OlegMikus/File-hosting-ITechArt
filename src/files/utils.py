7e28c058d2057f7af13214baecfbc9fc9f65b1f7
OlegMikus <aleh.mikus@itechart-group.com>
2021-10-22 17:13:41 +0300

    fix via linter

--- src/files/utils.py
+++ src/files/utils.py
@@ -1,13 +1,13 @@
 import hashlib
 
 
-def calculate_hash_md5(file):
-    h = hashlib.md5()
+def calculate_hash_md5(input_file):
+    md5 = hashlib.md5()
 
-    with open(file, 'rb') as file:
+    with open(input_file, 'rb') as file:
         chunk = 0
         while chunk != b'':
             chunk = file.read(1024)
-            h.update(chunk)
+            md5.update(chunk)
 
-    return h.hexdigest()
+    return md5.hexdigest()
