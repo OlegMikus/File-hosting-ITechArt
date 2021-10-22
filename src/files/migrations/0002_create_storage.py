b3e9a9075e9b13ac402fbe997c1244584828b7c2
OlegMikus <aleh.mikus@itechart-group.com>
2021-10-22 12:04:42 +0300

    improve migrations

--- src/files/migrations/0002_create_storage.py
+++ src/files/migrations/0002_create_storage.py
@@ -1,18 +1,19 @@
-from django.db import models, migrations
-from datetime import date
+from django.db import migrations
 
+from src.config.settings import BASE_DIR
 from src.files.constants import FILE_STORAGE__TYPE__PERMANENT, FILE_STORAGE__TYPE__TEMP
 
 
 def load_data(apps, schema_editor):
+
+    storage_base_dir = BASE_DIR / 'storage'
+    temp_storage_dir = storage_base_dir / 'temporary_storage'
+    permanent_storage_dir = storage_base_dir / 'permanent_storage'
+
     FilesStorage = apps.get_model('files', 'FilesStorage')
 
-    FilesStorage(type=FILE_STORAGE__TYPE__TEMP,
-                 destination='storage/temporary_storage/'
-                 ).save()
-    FilesStorage(type=FILE_STORAGE__TYPE__PERMANENT,
-                 destination='storage/permanent_storage/'
-                 ).save()
+    FilesStorage(type=FILE_STORAGE__TYPE__TEMP, destination=temp_storage_dir).save()
+    FilesStorage(type=FILE_STORAGE__TYPE__PERMANENT, destination=permanent_storage_dir).save()
 
 
 class Migration(migrations.Migration):
