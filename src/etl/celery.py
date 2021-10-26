ad3681a8adfe044b5c239b597009214be15eb570
OlegMikus <mikus.oleg@gmail.com>
2021-10-26 14:40:42 +0300

    refactor structure

--- src/etl/celery.py
+++ src/etl/celery.py
@@ -4,6 +4,6 @@ from celery import Celery
 
 celery_app = Celery()
 
-celery_app.config_from_object('src.config.celery_consts', namespace='CELERY')
+celery_app.config_from_object('src.etl.celery_consts', namespace='CELERY')
 
 celery_app.autodiscover_tasks()
