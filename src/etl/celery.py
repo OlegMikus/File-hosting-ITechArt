0d76d780e3f9605682563f92922209c5036ed241
OlegMikus <aleh.mikus@itechart-group.com>
2021-10-28 17:56:18 +0300

    add beat schedule

--- src/etl/celery.py
+++ src/etl/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.config.settings')
 
@@ -9,3 +10,10 @@ celery_app = Celery()
 celery_app.config_from_object('src.config.celery_consts', namespace='CELERY')
 
 celery_app.autodiscover_tasks()
+
+celery_app.conf.beat_schedule = {
+    'remove-chunks-after-week-of-keeping': {
+        'task': 'src.apps.files.tasks.rm_expired_chunks',
+        'schedule': crontab(day_of_week='*', hour=4, minute=0),
+    }
+}
