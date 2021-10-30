5e852fe392b5b711934edab1ee5f2b65385f3478
OlegMikus <mikus.oleg@gmail.com>
2021-10-30 22:06:54 +0300

    finish file format validator

--- src/apps/files/validators.py
+++ src/apps/files/validators.py
@@ -2,21 +2,10 @@ from typing import Any
 
 import magic
 
-JPG = 'image/jpeg'
-GIF = 'image/gif'
-TIFF = 'image/tiff'
-PNG = 'image/png'
-SVG = 'image/swg+xml'
-DOCX = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-XLS = ''
-PDF = 'application/pdf'
-TXT = 'text/plain'
-
-ALLOWED_FORMATS = {JPG, GIF, TIFF, PNG, SVG, DOCX, XLS, PDF, TXT}
+from src.apps.files.constants import ALLOWED_FORMATS
 
 
 def format_validator(value: Any) -> bool:
-    file = value.read(2048)
+    file = value.read(1024)
     file_type = magic.from_buffer(file, mime=True)
-    print(file_type)
     return file_type in ALLOWED_FORMATS
