f3bcd677ad98d66313faa6566f1b78af1b9352e0
OlegMikus <aleh.mikus@itechart-group.com>
2021-11-01 17:09:25 +0300

    change constants names

--- src/apps/files/constants.py
+++ src/apps/files/constants.py
@@ -3,14 +3,15 @@ FILE_STORAGE__TYPE__PERMANENT = 'permanent'
 FILE__NON_CHUNK__MAX_SIZE = 50 * 1024 * 1024
 CHUNKS__STORAGE_TIME__DAYS = 7
 
-JPG = 'image/jpeg'
-PNG = 'image/png'
-GIF = 'image/gif'
-TIFF = 'image/tiff'
-SVG = 'image/svg+xml'
-PDF = 'application/pdf'
-TXT = 'text/plain'
-XLS = 'application/vnd.ms-excel'
-DOCS = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+JPG__MIME_TYPE = 'image/jpeg'
+PNG__MIME_TYPE = 'image/png'
+GIF__MIME_TYPE = 'image/gif'
+TIFF__MIME_TYPE = 'image/tiff'
+SVG__MIME_TYPE = 'image/svg+xml'
+PDF__MIME_TYPE = 'application/pdf'
+TXT__MIME_TYPE = 'text/plain'
+XLS__MIME_TYPE = 'application/vnd.ms-excel'
+DOCS__MIME_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
 
-ALLOWED_FORMATS = {JPG, PDF, PNG, GIF, TIFF, SVG, TXT, XLS, DOCS}
+ALLOWED_FORMATS = {JPG__MIME_TYPE, PDF__MIME_TYPE, PNG__MIME_TYPE, GIF__MIME_TYPE, TIFF__MIME_TYPE, SVG__MIME_TYPE,
+                   TXT__MIME_TYPE, XLS__MIME_TYPE, DOCS__MIME_TYPE}
