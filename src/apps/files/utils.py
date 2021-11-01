5ce9a81df332f8debbcec44075a7c5b1da612ed9
OlegMikus <aleh.mikus@itechart-group.com>
2021-11-01 10:52:56 +0300

    move file format validator here

--- src/apps/files/utils.py
+++ src/apps/files/utils.py
@@ -2,7 +2,10 @@ import hashlib
 import os
 from typing import Dict, Any, List
 
+import magic
+
 from src.apps.accounts.models import User
+from src.apps.files.constants import ALLOWED_FORMATS
 from src.apps.files.models import FilesStorage, File
 
 
@@ -13,12 +16,10 @@ def get_chunk_name(filename: str, chunk_number: int) -> str:
 def calculate_hash_md5(file_path) -> str:
     md5 = hashlib.md5()
 
-    with open(file_path, 'rb') as file:
-        chunk = 0
-        while chunk != b'':
-            chunk = file.read(1024)
-            md5.update(chunk)
-
+    chunk = 0
+    while chunk != b'':
+        chunk = file_path.read(1024)
+        md5.update(chunk)
     return md5.hexdigest()
 
 
@@ -35,3 +36,10 @@ def create_file(user: User,
 
 def is_upload_complete(chunks_paths: List[str]) -> bool:
     return all([os.path.exists(path) for path in chunks_paths])
+
+
+def is_valid_format(file_path: str) -> bool:
+    with open(file_path, 'r') as users_file:
+        file = users_file.read(1024)
+        file_type = magic.from_buffer(file, mime=True)
+    return file_type in ALLOWED_FORMATS
