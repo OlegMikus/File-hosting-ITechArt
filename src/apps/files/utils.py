48f8d18f54a26fcdf621480372fba73a535872e0
OlegMikus <mikus.oleg@gmail.com>
2021-11-02 22:24:42 +0300

    make validators universal

--- src/apps/files/utils.py
+++ src/apps/files/utils.py
@@ -1,6 +1,6 @@
 import hashlib
 import os
-from typing import Dict, Any, List, BinaryIO
+from typing import Dict, Any, List, BinaryIO, Optional
 
 import magic
 
@@ -13,15 +13,35 @@ def get_chunk_name(filename: str, chunk_number: int) -> str:
     return f'{filename}_part_{chunk_number}'
 
 
-def is_valid_hash_md5(hash_sum: str, file_path: str) -> bool:
+def extract_file_path(file: Optional[str, File]) -> str:
+    file_path = None
+    if type(file) == str:
+        file_path = file
+    elif type(file) == File:
+        file_path = file.destination
+    return file_path
+
+
+def is_valid_hash_md5(hash_sum: str, file: Optional[str, BinaryIO, File]) -> bool:
     md5 = hashlib.md5()
-    chunk = 0
-    with open(file_path, 'rb') as file:
+    if type(file) == str or type(file) == File:
+        file_path = extract_file_path(file)
+        with open(file_path, 'rb') as file:
+            chunk = 0
+            while chunk != b'':
+                chunk = file.read(1024)
+                md5.update(chunk)
+            return hash_sum == md5.hexdigest()
+
+    elif type(file) == BinaryIO:
+        chunk = 0
         while chunk != b'':
             chunk = file.read(1024)
             md5.update(chunk)
+        return hash_sum == md5.hexdigest()
 
-    return hash_sum == md5.hexdigest()
+    else:
+        return False
 
 
 def create_file(user: User,
@@ -39,8 +59,18 @@ def is_upload_complete(chunks_paths: List[str]) -> bool:
     return all([os.path.exists(path) for path in chunks_paths])
 
 
-def is_valid_format(file_path: str) -> bool:
-    with open(file_path, 'r') as users_file:
-        file = users_file.read(1024)
-        file_type = magic.from_buffer(file, mime=True)
-    return file_type in ALLOWED_FORMATS
+def is_valid_format(file: Optional[str, BinaryIO, File]) -> bool:
+
+    if type(file) == str or type(file) == File:
+        file_path = extract_file_path(file)
+        with open(file_path, 'r') as users_file:
+            file = users_file.read(1024)
+            file_type = magic.from_buffer(file, mime=True)
+            return file_type in ALLOWED_FORMATS
+
+    elif type(file) == BinaryIO:
+        file_type = magic.from_buffer(file.read(1024), mime=True)
+        return file_type in ALLOWED_FORMATS
+
+    else:
+        return False
