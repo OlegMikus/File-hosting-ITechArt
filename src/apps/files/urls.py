09e6e3728c1017a0121ca8adec848757fb4ca1d2
OlegMikus <aleh.mikus@itechart-group.com>
2021-11-03 16:15:22 +0300

    add download all users files url

--- src/apps/files/urls.py
+++ src/apps/files/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from src.apps.files.views.download_all_users_files import AllUsersFilesDownload
 from src.apps.files.views.file_build_view import BuildFileView
 from src.apps.files.views.upload import UploadView
 from src.apps.files.views.upload_template import UploadTemplateView
@@ -9,5 +10,6 @@ urlpatterns = [
     path('upload/', UploadTemplateView.as_view(), name='upload-template'),
     path('upload/chunks/', UploadView.as_view(), name='upload-chunks'),
     path('build/', BuildFileView.as_view(), name='build'),
-    path('upload/non-chunk/', NonChunkUploadView.as_view(), name='upload-non-chunk')
+    path('upload/non-chunk/', NonChunkUploadView.as_view(), name='upload-non-chunk'),
+    path('download/all-files/', AllUsersFilesDownload.as_view(), name='download-all-files')
 ]
