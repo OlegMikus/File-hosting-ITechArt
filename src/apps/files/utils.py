0bf5f5ddf14438c81a5c1bf7e6a8f52b6ce42c81
OlegMikus <aleh.mikus@itechart-group.com>
2021-11-03 12:59:59 +0300

    change hash calculator util

--- src/apps/files/utils.py
+++ src/apps/files/utils.py
@@ -13,44 +13,26 @@ def get_chunk_name(filename: str, chunk_number: int) -> str:
     return f'{filename}_part_{chunk_number}'
 
 
-def extract_file_path(file: Union[str, File]) -> str:
-    file_path = None
-    if type(file) == str:
-        file_path = file
-    elif type(file) == File:
-        file_path = file.destination
-    return file_path
-
-
-def is_valid_hash_md5(hash_sum: str, file: Union[str, BinaryIO, File]) -> bool:
+def is_valid_hash_md5(hash_sum: str, file_path: str) -> bool:
     md5 = hashlib.md5()
-    if type(file) == str or type(file) == File:
-        file_path = extract_file_path(file)
+    if file_path:
         with open(file_path, 'rb') as file:
             chunk = 0
             while chunk != b'':
                 chunk = file.read(1024)
                 md5.update(chunk)
             return hash_sum == md5.hexdigest()
-
-    elif type(file) == BinaryIO:
-        chunk = 0
-        while chunk != b'':
-            chunk = file.read(1024)
-            md5.update(chunk)
-        return hash_sum == md5.hexdigest()
-
     else:
         return False
 
 
 def create_file(user: User,
                 storage: FilesStorage,
-                file_path: str,
+                user_storage_dir: str,
                 data: Dict[str, Any]
                 ) -> None:
     File.objects.create(user=user, storage=storage,
-                        destination=file_path, name=data.get('filename'),
+                        destination=user_storage_dir, name=data.get('filename'),
                         description=data.get('description'), type=data.get('extension'),
                         size=data.get('total_size'), hash=data.get('hash_sum'))
 
@@ -59,18 +41,11 @@ def is_upload_complete(chunks_paths: List[str]) -> bool:
     return all([os.path.exists(path) for path in chunks_paths])
 
 
-def is_valid_format(file: Union[str, BinaryIO, File]) -> bool:
-
-    if type(file) == str or type(file) == File:
-        file_path = extract_file_path(file)
+def is_valid_format(file_path: str) -> bool:
+    if file_path:
         with open(file_path, 'r') as users_file:
             file = users_file.read(1024)
             file_type = magic.from_buffer(file, mime=True)
             return file_type in ALLOWED_FORMATS
-
-    elif type(file) == BinaryIO:
-        file_type = magic.from_buffer(file.read(1024), mime=True)
-        return file_type in ALLOWED_FORMATS
-
     else:
         return False
