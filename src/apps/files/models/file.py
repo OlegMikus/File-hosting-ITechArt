8b969726f973859c72085e9fab8988e2ade59a9d
OlegMikus <aleh.mikus@itechart-group.com>
2021-11-03 13:38:57 +0300

    rename function add typing

--- src/apps/files/models/file.py
+++ src/apps/files/models/file.py
@@ -22,5 +22,5 @@ class File(BaseModel):
         db_table = 'file'
 
     @property
-    def get_absolute_path(self):
+    def absolute_path(self):
         return self.storage.destination / self.destination
