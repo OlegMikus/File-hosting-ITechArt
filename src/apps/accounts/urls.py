750e3b287c2c36bf1692cd938b7fb83e424c1414
OlegMikus <aleh.mikus@itechart-group.com>
2021-11-05 09:58:01 +0300

    add url for user profile

--- src/apps/accounts/urls.py
+++ src/apps/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from src.apps.accounts.views.login import UserLoginView
+from src.apps.accounts.views.profile import ProfileView
 from src.apps.accounts.views.signup import UserSignUpView
 from src.apps.accounts.views.refresh import RefreshView
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path('signup/', UserSignUpView.as_view(), name='signup'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('refresh/', RefreshView.as_view(), name='refresh'),
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
