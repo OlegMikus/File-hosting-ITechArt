a40e2eb8f952b0501521f8b416117c47d7c42b69
OlegMikus <aleh.mikus@itechart-group.com>
2021-11-05 11:46:35 +0300

    add change password url

--- src/apps/accounts/urls.py
+++ src/apps/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from src.apps.accounts.views.change_password import ChangePasswordView
 from src.apps.accounts.views.login import UserLoginView
 from src.apps.accounts.views.profile import ProfileView
 from src.apps.accounts.views.signup import UserSignUpView
@@ -10,4 +11,5 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('refresh/', RefreshView.as_view(), name='refresh'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('change-password/', ChangePasswordView.as_view(), name='change-password')
 ]
