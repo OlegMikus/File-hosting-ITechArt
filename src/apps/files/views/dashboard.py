2b93f56a7318ed9bbca2e5bb30a5bd2fcaaca68f
OlegMikus <aleh.mikus@itechart-group.com>
2021-11-05 08:52:16 +0300

    minor fix

--- src/apps/files/views/dashboard.py
+++ src/apps/files/views/dashboard.py
@@ -16,11 +16,10 @@ class DashboardView(GenericAPIView):
     ordering_fields = ('date_updated', 'name', 'size')
     pagination_class = PageNumberPagination
     filter_backends = OrderingFilter
-    pagination_class.page_size = 1
+    pagination_class.page_size = 4
 
-    # @login_required
-    def get(self, request: Request, *args: Any, **kwargs: Any):
-        user = User.objects.get(id='7eb129d1-1de5-42f4-9116-b5274705a8ec')
+    @login_required
+    def get(self, request: Request, *args: Any, user: User, **kwargs: Any) -> OkResponse:
         queryset = File.objects.filter(user=user)
         serializer = FileSerializer(queryset, many=True)
         page = self.paginate_queryset(serializer.data)
