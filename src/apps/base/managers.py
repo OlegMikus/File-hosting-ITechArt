42b0dfb6d3d0720fe800e3742b82ebe324246078
OlegMikus <mikus.oleg@gmail.com>
2021-11-05 23:44:34 +0300

    add typing

--- src/apps/base/managers.py
+++ src/apps/base/managers.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.db.models import QuerySet
 
 
 class IsAliveObjectsManager(models.Manager):
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet:
         return super().get_queryset().filter(is_alive=True)
 
 
 class AllObjectsManager(models.Manager):
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet:
         return super().get_queryset().all()
