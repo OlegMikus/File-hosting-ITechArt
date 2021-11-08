5a3bf35a98ef45901ba33a35c25f28613bf5ae3a
OlegMikus <aleh.mikus@itechart-group.com>
2021-11-08 18:43:34 +0300

    git test of user creation

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,5 +5,7 @@ from src.apps.accounts.models import User
 
 @pytest.mark.django_db
 def test_temp() -> None:
-    User.objects.create_user(username='john', email='lennon@thebeatles.com', first_name='johnpassword')
+    User.objects.create_user(username='john', email='lennon@thebeatles.com',
+                             first_name='johnpassword',
+                             password='Admin@12432')
     assert User.objects.count() == 1
