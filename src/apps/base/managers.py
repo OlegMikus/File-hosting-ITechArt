d35568b628b3591e5caba22ce806609a5db1d87a
OlegMikus <aleh.mikus@itechart-group.com>
2021-11-08 15:36:00 +0300

    change naming

--- src/apps/base/managers.py
+++ src/apps/base/managers.py
@@ -2,22 +2,22 @@ from django.db import models
 from django.db.models import QuerySet
 
 
-class BaseQuerySet(models.query.QuerySet):
+class SoftDeleteQuerySet(models.query.QuerySet):
     """
     QuerySet whose delete() does not delete items, but instead marks the rows as not alive
     """
     def delete(self) -> None:
         self.update(is_alive=False)
 
-    def only_alive(self) -> QuerySet:
+    def get_alive(self) -> QuerySet:
         return self.filter(is_alive=True)
 
 
 class IsAliveObjectsManager(models.Manager):
     def get_queryset(self) -> QuerySet:
-        return BaseQuerySet(self.model).only_alive()
+        return SoftDeleteQuerySet(self.model).get_alive()
 
 
 class AllObjectsManager(models.Manager):
     def get_queryset(self) -> QuerySet:
-        return BaseQuerySet(self.model)
+        return SoftDeleteQuerySet(self.model)
