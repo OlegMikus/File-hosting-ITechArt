ee6132b15e0396fdf110f69fb82fe88313876b48
OlegMikus <aleh.mikus@itechart-group.com>
2021-11-09 10:02:02 +0300

    add task to remole files with is alive false

--- src/apps/files/tasks.py
+++ src/apps/files/tasks.py
@@ -6,7 +6,7 @@ from typing import List, Any, Dict
 
 from src.apps.accounts.models import User
 from src.apps.files.constants import FILE_STORAGE__TYPE__TEMP, CHUNKS__STORAGE_TIME__DAYS, ALLOWED_FORMATS
-from src.apps.files.models import FilesStorage
+from src.apps.files.models import FilesStorage, File
 from src.apps.files.utils import create_file, is_valid_format, is_valid_hash_md5
 
 from src.etl.celery import celery_app
@@ -42,6 +42,20 @@ def task_build_file(user_id: str,
         # TODO: send_mail() with errors, function here, will be created in another branch
         return None
     create_file(user, file_storage, data)
+
+
+@celery_app.task
+def remove_files_with_deleted_mark(file_path: str) -> None:
+    shutil.rmtree(file_path)
+
+
+@celery_app.task
+def remove_files_with_deleted_mark_once_per_month() -> None:
+    queryset = File.all_objects.filter(is_alive=False)
+    for file in queryset:
+        if not os.path.exists(file.absolute_path) or not os.path.isfile(file.absolute_path):
+            continue
+        shutil.rmtree(file.absolute_path)
 
 
 @celery_app.task
--- src/etl/celery.py
+++ src/etl/celery.py
@@ -15,5 +15,9 @@ celery_app.conf.beat_schedule = {
     'remove_expired_chunks': {
         'task': 'src.apps.files.tasks.remove_expired_chunks',
         'schedule': crontab(day_of_week='*', hour=4, minute=0),
+    },
+    'remove_files_with_deleted_mark_once_per_month': {
+        'task': 'src.apps.files.tasks.remove_files_with_deleted_mark_once_per_month',
+        'schedule': crontab(month_of_year='*', day_of_month='1', hour=5, minute=0),
     }
 }
