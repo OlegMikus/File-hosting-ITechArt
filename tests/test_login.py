4077020a6d3f756e47fc6249533a72e719dcfb0a
OlegMikus <aleh.mikus@itechart-group.com>
2021-11-09 10:28:46 +0300

    add new test

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,8 +4,18 @@ from src.apps.accounts.models import User
 
 
 @pytest.mark.django_db
-def test_temp() -> None:
-    User.objects.create_user(username='john', email='lennon@thebeatles.com',
-                             first_name='johnpassword',
-                             password='Admin@12432')
-    assert User.objects.count() == 1
+class TestModels:
+
+    def test_user_creation(self) -> None:
+        User.objects.create_user(username='test_user', email='testuser@test.com',
+                                 first_name='test_name',
+                                 password='Admin@12432')
+        assert User.objects.count() == 1
+
+    def test_user_deletion(self) -> None:
+        User.objects.create_user(username='test_user', email='testuser@test.com',
+                                 first_name='test_name',
+                                 password='Admin@12432')
+        user = User.objects.get(username='test_user')
+        user.delete()
+        assert user.is_alive is False
