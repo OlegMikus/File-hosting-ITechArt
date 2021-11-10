a512361da49d27b042538ef789281227e3a4c5fe
OlegMikus <aleh.mikus@itechart-group.com>
2021-11-10 13:01:06 +0300

    fix tasks names

--- src/etl/celery.py
+++ src/etl/celery.py
@@ -12,11 +12,11 @@ celery_app.config_from_object('src.config.celery_consts', namespace='CELERY')
 celery_app.autodiscover_tasks()
 
 celery_app.conf.beat_schedule = {
-    'remove_expired_chunks': {
-        'task': 'src.apps.files.tasks.task.remove_expired_chunks',
+    'task_remove_expired_chunks': {
+        'task': 'src.apps.files.tasks.task.task_remove_expired_chunks',
         'schedule': crontab(day_of_week='*', hour=4, minute=0),
     },
-    'remove_files_with_deleted_mark_once_per_month': {
+    'task_remove_deleted_files': {
         'task': 'src.apps.files.tasks.task_remove_deleted_files',
         'schedule': crontab(month_of_year='*', day_of_month='1', hour=5, minute=0),
     }
