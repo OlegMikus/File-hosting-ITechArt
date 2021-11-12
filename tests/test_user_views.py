5ab8d3a45d0c95772371e548814cc9f80136d28c
OlegMikus <aleh.mikus@itechart-group.com>
2021-11-12 10:27:07 +0300

    split classes, add typing

--- tests/test_user_views.py
+++ tests/test_user_views.py
@@ -1,23 +1,24 @@
-from typing import Tuple
+from typing import Callable
 
 import pytest
 from django.urls import reverse
 
 from src.apps.accounts.models import User
 from src.apps.accounts.serializers.user_serializer import UserSerializer
+from rest_framework.test import APIClient
 
 
 @pytest.mark.django_db
 class TestUserSignupView:
 
-    def test_signup_view(self, api_client) -> None:
+    def test_signup_view(self, api_client: APIClient) -> None:
         url = reverse('signup')
         data = {'username': 'TestUser',
                 'password': 'A23$#fsfrwfwe'}
         response = api_client.post(url, data=data)
         assert response.status_code == 201
 
-    def test_bad_signup_view(self, api_client) -> None:
+    def test_bad_signup_view(self, api_client: APIClient) -> None:
         url = reverse('signup')
         data = {'username': 'TestUser'}
         response = api_client.post(url, data=data)
@@ -27,7 +28,7 @@ class TestUserSignupView:
 
 class TestUserLoginView:
 
-    def test_login_view(self, api_client, create_user) -> None:
+    def test_login_view(self, api_client: APIClient, create_user: Callable) -> None:
         url = reverse('login')
         user = create_user()
         data = {'username': user.username, 'password': 'Afe3#@vdfvrrcvs'}
@@ -35,7 +36,7 @@ class TestUserLoginView:
         assert response.status_code == 200
         assert response.data['data']['result']['id'] == str(user.id)
 
-    def test_bad_login_view(self, api_client, create_user) -> None:
+    def test_bad_login_view(self, api_client: APIClient, create_user: Callable) -> None:
         url = reverse('login')
         user = create_user()
         data = {'username': user.username, 'password': 'dfkoijgdpfog'}
@@ -45,34 +46,36 @@ class TestUserLoginView:
 
 
 class TestUserRefreshView:
-    def test_refresh_view(self, api_client, create_token_for_user, create_user) -> None:
+    def test_refresh_view(self, api_client: APIClient, create_token_for_user: Callable, create_user: Callable) -> None:
         url = reverse('refresh')
         api_client.credentials(HTTP_Refresh_Token=create_token_for_user(create_user()))
         response = api_client.get(url)
         assert response.status_code == 200
 
-    def test_bad_refresh_view(self, api_client) -> None:
+    def test_bad_refresh_view(self, api_client: APIClient) -> None:
         url = reverse('refresh')
         response = api_client.get(url)
-        assert response.status_code == 400
+        assert response.status_code == 401
         assert response.data['data']['error_detail'] == ('Missing token',)
 
 
 class TestUserProfileView:
 
-    def test_profile_get_view(self, create_token_for_user, create_user, api_client) -> None:
+    def test_profile_get_view(self, create_token_for_user: Callable, create_user: Callable,
+                              api_client: APIClient) -> None:
         url = reverse('profile')
         api_client.credentials(HTTP_Access_Token=create_token_for_user(create_user()))
         response = api_client.get(url)
         assert response.status_code == 200
 
-    def test_bad_profile_get_view(self, api_client) -> None:
+    def test_bad_profile_get_view(self, api_client: APIClient) -> None:
         url = reverse('profile')
         response = api_client.get(url)
-        assert response.status_code == 400
+        assert response.status_code == 401
         assert response.data['data']['error_detail'] == ('Missing token',)
 
-    def test_profile_put_view(self, api_client, create_token_for_user, create_user) -> None:
+    def test_profile_put_view(self, api_client: APIClient, create_token_for_user: Callable,
+                              create_user: Callable) -> None:
         url = reverse('profile')
         user = create_user()
         api_client.credentials(HTTP_Access_Token=create_token_for_user(user))
@@ -84,18 +87,19 @@ class TestUserProfileView:
         user = User.objects.get(id=user.id)
         assert response.data['data']['result'] == UserSerializer(user).data
 
-    def test_bad_profile_put_view(self, api_client, create_token_for_user: Tuple[str, User]) -> None:
+    def test_bad_profile_put_view(self, api_client: APIClient) -> None:
         url = reverse('profile')
         api_client.credentials(HTTP_Access_Token=None)
         response = api_client.get(url)
-        assert response.status_code == 400
+        assert response.status_code == 401
         assert response.data['data']['result'] is None
         assert response.data['data']['error_detail'] == ('Missing token',)
 
 
 class TestUserChangePasswordView:
 
-    def test_change_password_view(self, api_client, create_user, create_token_for_user) -> None:
+    def test_change_password_view(self, api_client: APIClient, create_user: Callable,
+                                  create_token_for_user: Callable) -> None:
         url = reverse('change-password')
 
         api_client.credentials(HTTP_Access_Token=create_token_for_user(create_user()))
@@ -107,7 +111,8 @@ class TestUserChangePasswordView:
         response = api_client.put(url, data)
         assert response.status_code == 200
 
-    def test_bad_change_password_view(self, api_client, create_user, create_token_for_user) -> None:
+    def test_bad_change_password_view(self, api_client: APIClient, create_user: Callable,
+                                      create_token_for_user: Callable) -> None:
         url = reverse('change-password')
 
         api_client.credentials(HTTP_Access_Token=create_token_for_user(create_user()))
