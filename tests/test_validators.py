8e98f514b8cd993b0c7f82f9b26c1a8755905e2e
OlegMikus <aleh.mikus@itechart-group.com>
2021-11-12 10:37:45 +0300

    change fail test logic

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -1,4 +1,5 @@
 from rest_framework.exceptions import ValidationError
+import pytest
 
 from src.apps.accounts.validators import validate_age, validate_name, validate_password, username_validator
 
@@ -7,35 +8,27 @@ class TestValidators:
     def test_age_validator(self) -> None:
         assert validate_age(50) is None
 
-    def test_false_age_validator(self) -> None:
-        try:
+    def test_age_validator_fail(self) -> None:
+        with pytest.raises(ValidationError):
             validate_age(150)
-        except ValidationError:
-            assert True
 
     def test_name_validator(self) -> None:
         assert validate_name('Test_name') is None
 
-    def test_false_name_validator(self) -> None:
-        try:
+    def test_name_validator_fail(self) -> None:
+        with pytest.raises(ValidationError):
             validate_name('Name1')
-        except ValidationError:
-            assert True
 
     def test_password_validator(self) -> None:
         assert validate_password('A1@qwertyuio') is None
 
-    def test_false_password_validator(self) -> None:
-        try:
+    def test_password_validator_fail(self) -> None:
+        with pytest.raises(ValidationError):
             validate_password('password')
-        except ValidationError:
-            assert True
 
     def test_username_validator(self) -> None:
         assert username_validator('Username@.+-_') is None
 
-    def test_false_username_validator(self) -> None:
-        try:
+    def test_username_validator_fail(self) -> None:
+        with pytest.raises(ValidationError):
             username_validator('User$%$name')
-        except ValidationError:
-            assert True
