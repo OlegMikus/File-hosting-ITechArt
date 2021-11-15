e12e28ef0fd08a0ee3748a301eda249535093f80
OlegMikus <aleh.mikus@itechart-group.com>
2021-11-15 18:02:11 +0300

    separate apps for containers

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path, include
 
-urlpatterns = [
-    path('api/users/', include('src.apps.accounts.urls')),
-    path('api/files/', include('src.apps.files.urls')),
-]
+from src.config.env_consts import APP_TYPE
+
+urlpatterns = []
+
+if APP_TYPE == 'AUTH':
+    urlpatterns = [
+        path('api/users/', include('src.apps.accounts.urls')),
+    ]
+
+if APP_TYPE == 'FILE':
+    urlpatterns = [
+        path('api/files/', include('src.apps.files.urls')),
+    ]
