d598e5848a88377f3695d10e650fe93a3146ca9c
OlegMikus <aleh.mikus@itechart-group.com>
2021-11-16 11:47:24 +0300

    fix bugs

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path, include
 
-from src.config.env_consts import APP_TYPE
+from src.config.env_consts import APP_TYPE, APP_TYPE_FILE, APP_TYPE_AUTH
 
 urlpatterns = []
 
-if APP_TYPE == 'AUTH':
+if APP_TYPE == APP_TYPE_AUTH:
     urlpatterns = [
         path('api/users/', include('src.apps.accounts.urls')),
     ]
 
-if APP_TYPE == 'FILE':
+if APP_TYPE == APP_TYPE_FILE:
     urlpatterns = [
         path('api/files/', include('src.apps.files.urls')),
     ]
