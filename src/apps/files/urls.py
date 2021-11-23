f3cc2e3f97e9e09d4e41ffb774a2a9d087bfb299
OlegMikus <aleh.mikus@itechart-group.com>
2021-11-23 15:25:52 +0300

    rm view with template render

--- src/apps/files/urls.py
+++ src/apps/files/urls.py
@@ -6,11 +6,9 @@ from src.apps.files.views.download_all_users_files import AllUsersFilesDownload
 from src.apps.files.views.download_single_file import FileDownloadView
 from src.apps.files.views.file_build_view import BuildFileView
 from src.apps.files.views.upload import UploadView
-from src.apps.files.views.upload_template import UploadTemplateView
 from src.apps.files.views.upload_non_chunk import NonChunkUploadView
 
 urlpatterns = [
-    path('upload/', UploadTemplateView.as_view(), name='upload-template'),
     path('upload/chunks/', UploadView.as_view(), name='upload-chunks'),
     path('build/', BuildFileView.as_view(), name='build'),
     path('upload/non-chunk/', NonChunkUploadView.as_view(), name='upload-non-chunk'),
