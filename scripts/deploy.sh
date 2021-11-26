693896008f3cce15d20d64db335f74fd50ebba55
OlegMikus <aleh.mikus@itechart-group.com>
2021-11-26 12:40:47 +0300

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,13 @@ set -o errexit # exit script when command fails
 set -o pipefail # this setting prevents errors in a pipeline from being masked
 set -o nounset # exit script when it tries to use undeclared variables
 
+# INIT WORKING DIR
+# ===================================================
+cd "$(dirname "${BASH_SOURCE[0]}")"
+FILE_DIR=$(pwd)
+cd ..
+CWD="$(pwd)"
+
 
 zip -r  proj.zip ./src/ ./srv/ ./docker-compose.yml ./Pipfile ./Pipfile.lock ./manage.py ./.env ./static/ ./entrypoint.sh
 
@@ -14,3 +21,6 @@ ssh root@134.122.78.76 'cd /home/oleg/file-hosting && sudo rm -r ./src/ ./srv/ .
 scp proj.zip root@134.122.78.76:/home/oleg/
 
 ssh root@134.122.78.76 'sudo unzip /home/oleg/proj.zip -d /home/oleg/file-hosting'
+
+ssh oleg@134.122.78.76 'cd file-hosting/ && docker-compose down'
+ssh oleg@134.122.78.76 'cd file-hosting/ && docker-compose up'
