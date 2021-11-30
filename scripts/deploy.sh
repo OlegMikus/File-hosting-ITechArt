ed80b0f903ab808161deedac0239ecee4aee74bf
OlegMikus <aleh.mikus@itechart-group.com>
2021-11-30 10:49:25 +0300

    finish deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,8 +13,13 @@ FILE_DIR=$(pwd)
 cd ..
 CWD="$(pwd)"
 
+chmod ug+x "./scripts/lint.sh"
+bash ./scripts/lint.sh
+bash ./scripts/tests.sh
 
 zip -r  proj.zip ./src/ ./srv/ ./docker-compose.yml ./Pipfile ./Pipfile.lock ./manage.py ./.env ./static/ ./entrypoint.sh
+
+ssh oleg@134.122.78.76 'cd file-hosting/ && docker-compose down'
 
 ssh root@134.122.78.76 'cd /home/oleg/file-hosting && sudo rm -r ./src/ ./srv/ ./docker-compose.yml ./Pipfile ./Pipfile.lock ./manage.py ./.env ./static/ ./entrypoint.sh'
 
@@ -22,5 +27,5 @@ scp proj.zip root@134.122.78.76:/home/oleg/
 
 ssh root@134.122.78.76 'sudo unzip /home/oleg/proj.zip -d /home/oleg/file-hosting'
 
-ssh oleg@134.122.78.76 'cd file-hosting/ && docker-compose down'
-ssh oleg@134.122.78.76 'cd file-hosting/ && docker-compose up'
+ssh oleg@134.122.78.76 'cd file-hosting/ && docker-compose up --build'
+ssh oleg@134.122.78.76 'docker rm $(docker ps --filter status=exited -q)'
